a07d8ac5d057a53f07503dece80f0f5fa7730d07
[zhangyunze-280] <[zhangyunze@cy-com.co.jp]>
2025-06-09 17:18:44 +0900

    サービスの作成

--- src/main/java/com/example/patient_system/service/PatientService.java
+++ src/main/java/com/example/patient_system/service/PatientService.java
@@ -1,7 +1,24 @@
 package com.example.patient_system.service;
 
+import com.example.patient_system.entity.Patient;
+import com.example.patient_system.repository.PatientRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 
+@Service
 public class PatientService {
-    
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    // ここに必要に応じて患者の追加、更新、削除、詳細取得のメソッドを追加していく
+
 }
