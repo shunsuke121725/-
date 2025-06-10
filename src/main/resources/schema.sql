15d0ce06a79ca662f4201f2f40f31c83fa174f6d
[zhangyunze-280] <[zhangyunze@cy-com.co.jp]>
2025-06-10 12:29:08 +0900

    データベースの修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,18 @@
-create table lists (
-    list_id integer auto_increment primary key,
-    name varchar(100) not null,
-    latestBookingDate date,
-    place varchar (100)
+CREATE TABLE lists (
+    list_id INTEGER auto_increment PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    latestBookingDate DATE,
+    place varchar(100)
 );
 
-create table patients (
-    patient_id integer auto_increment primary key,
-    name varchar(100) not null,
-    age char(3) not null,
-    bloodType char(2),
+CREATE TABLE patients (
+    patient_id INTEGER auto_increment PRIMARY KEY,
+    list_id INTEGER NOT NULL,
+    name varchar(100) NOT NULL,
+    age CHAR(3) NOT NULL,
+    bloodType CHAR(2),
     diseaseName varchar(100),
     symptoms varchar(100),
-    latestBookingDate date,
-    foreign key (patient_id) references lists(list_id)
+    latestBookingDate DATE,
+    FOREIGN KEY (list_id) REFERENCES lists(list_id)
 );
