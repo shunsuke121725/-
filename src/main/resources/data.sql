c9a134f851b41ea8eea99470f52fb25736980c42
[zhangyunze-280] <[zhangyunze@cy-com.co.jp]>
2025-06-10 12:15:47 +0900

    データベースの修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into lists (name, latestBookingDate, plase) values
+insert into lists (name, latestBookingDate, place) values
    ('山田 太郎', '2025-06-01', '診察室A');
 
 insert into patients (name, age, bloodType, diseaseName, symptoms, latestBookingDate) values
