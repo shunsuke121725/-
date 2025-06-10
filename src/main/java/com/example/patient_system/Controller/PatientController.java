90f5308269167ff9160a2a33f44d919e03d28193
shunsuke121725 <chibashun@cy-com.co.jp>
2025-06-10 12:19:29 +0900

    画面一覧

--- src/main/java/com/example/patient_system/Controller/PatientController.java
+++ src/main/java/com/example/patient_system/Controller/PatientController.java
@@ -2,7 +2,17 @@ package com.example.patient_system.Controller;
 
 import com.example.patient_system.entity.Patient;
 import com.example.patient_system.service.PatientService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/patients")
 public class PatientController{
 
 }
