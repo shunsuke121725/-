3c3049d387ea163bd3949c7229de41204d9c6512
shunsuke121725 <chibashun@cy-com.co.jp>
2025-06-10 16:29:15 +0900

    更新

--- src/main/java/com/example/patient_system/mapper/PatientMapper.java
+++ src/main/java/com/example/patient_system/mapper/PatientMapper.java
@@ -7,17 +7,8 @@ import java.util.List;
 
 @Mapper
 public interface PatientMapper {
-<<<<<<< HEAD
 
     @Select("SELECT name, age, bloodType, diseaseName, symptoms, latestBookingDate FROM patients")
     List<Patient> selectALLPatients();
     
-=======
-   // 全患者の一覧を取得
-    @Select("SELECT * FROM patients")
-    List<Patient> findAll();
-    // patientIdで患者を取得
-    @Select("SELECT * FROM patients WHERE patient_id = #{patientId}")
-    Patient findById(Integer patientId);
->>>>>>> 29d701984f04c98e8b453d4ff108e996ef1af6b5
 }
